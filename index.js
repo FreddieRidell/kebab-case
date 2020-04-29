b4f2d5330933713686aeb52e2622416153839c15
Freddie Ridell <contact@freddieridell.com>
2020-04-29 19:44:47 +0100

    added real world test case

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-module.exports=x=>x.trim().replace(/[^ \-a-zA-Z0-9]/g, "").replace(/([a-z])([A-Z])/g,(_, x,y)=>x+"-"+y.toLowerCase()).toLowerCase().replace(/\s+/g, "-");
+module.exports = (x) =>
+  x
+    .trim()
+    .replace(/[^ \-a-zA-Z0-9]/g, "")
+    .replace(/([a-z])([A-Z])/g, (_, x, y) => x + "-" + y)
+    .toLowerCase()
+    .replace(/[\-\s]+/g, "-");
--- test.js
+++ test.js
@@ -1,23 +1,31 @@
 const k = require(".");
 
 describe("kebab-case", () => {
-	it("doens't change a string already in kebab case", () => {
-		expect(k("foo-bar-baz")).toBe("foo-bar-baz")
-	})
+  it("doens't change a string already in kebab case", () => {
+    expect(k("foo-bar-baz")).toBe("foo-bar-baz");
+  });
 
-	it("replaces spaces with '-'", () => {
-		expect(k("foo bar baz")).toBe("foo-bar-baz")
-	})
+  it("replaces spaces with '-'", () => {
+    expect(k("foo bar baz")).toBe("foo-bar-baz");
+  });
 
-	it("strips any non-alphanumeric characters", () => {
-		expect(k("1foo ' 2 bar *_ b3az")).toBe("1foo-2-bar-b3az")
-	})
-	it("replaces capital transitions in camelCase and PascalCase", () => {
-		expect(k("fooBarBaz")).toBe("foo-bar-baz")
-		expect(k("FooBarBaz")).toBe("foo-bar-baz")
-	});
-	it("doesn't add a '-' at the start or end of a string", () => {
-		expect(k("   foo bar baz   ")).toBe("foo-bar-baz")
-		expect(k("   FooBarBaz   ")).toBe("foo-bar-baz")
-	});
-})
+  it("strips any non-alphanumeric characters", () => {
+    expect(k("1foo ' 2 bar *_ b3az")).toBe("1foo-2-bar-b3az");
+  });
+  it("replaces capital transitions in camelCase and PascalCase", () => {
+    expect(k("fooBarBaz")).toBe("foo-bar-baz");
+    expect(k("FooBarBaz")).toBe("foo-bar-baz");
+  });
+  it("doesn't add a '-' at the start or end of a string", () => {
+    expect(k("   foo bar baz   ")).toBe("foo-bar-baz");
+    expect(k("   FooBarBaz   ")).toBe("foo-bar-baz");
+  });
+
+  [["Apt 308 - Drag It Yourself", "apt-308-drag-it-yourself"]].forEach(
+    ([input, output]) => {
+      it(`converts "${input}" to "${output}"`, () => {
+        expect(k(input)).toBe(output);
+      });
+    }
+  );
+});
